34ba7de5455eb35d926ada455df71115b37d5f61
Skylar <jczhang25@uwcchina.org>
2026-09-11 13:56:54 +0800

    userimput

---
+++ t04userinput.py
@@ -0,0 +1,8 @@
+name = input("What is your name?: ")
+age = int(input("What is your age?: "))
+
+age = age + 1
+
+print("Hello!")
+print(f"Happy birthday, {name}!")
+print(f"You are {age} years old.")
