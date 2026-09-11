ce586cfd6bb5231aed0028dbbd11df7abb4c4aa8
Skylar <jczhang25@uwcchina.org>
2026-09-11 13:38:20 +0800

    typecasting

--- helloworld.py
+++ helloworld.py
@@ -2,10 +2,9 @@ import random
 
 colors = ["\033[91m","\033[92m","\033[93m","\033[94m","\033[95m","\033[96m"]
 end = "\033[0m"
-msg = "✨Hello World✨"
+msg = "✨Hello Skylar!✨"
 
 for char in msg:
     c = random.choice(colors)
     print(c + char + end, end="")
 print()
-print("Hello Skylar!")
